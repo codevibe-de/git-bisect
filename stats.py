69db3f152b4b86f8891d64cb9a6e2a9bbafe9faf
Thomas Auinger <thomas@auinger.de>
2026-07-14 15:50:56 +0200

    Add find_min()

--- stats.py
+++ stats.py
@@ -12,7 +12,16 @@ def calculate_average(numbers):
     return calculate_sum(numbers) / len(numbers)
 
 
+def find_min(numbers):
+    result = numbers[0]
+    for n in numbers[1:]:
+        if n < result:
+            result = n
+    return result
+
+
 if __name__ == "__main__":
     data = [4, 8, 15, 16, 23, 42]
     print(f"Sum     : {calculate_sum(data)}")
     print(f"Average : {calculate_average(data)}")
+    print(f"Min     : {find_min(data)}")
